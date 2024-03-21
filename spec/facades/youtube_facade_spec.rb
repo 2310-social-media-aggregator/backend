3cb195bfbaf14b71a0bca45059b49e5b68e0ff6f
Quin Nordmark <quinnordmark@gmail.com>
2024-03-21 10:03:37 -0600

    creator_show_spec

--- spec/facades/youtube_facade_spec.rb
+++ spec/facades/youtube_facade_spec.rb
@@ -9,7 +9,7 @@ RSpec.describe 'YoutubeFacade' do
                     'Accept-Encoding'=>'gzip;q=1.0,deflate;q=0.6,identity;q=0.3',
                     'User-Agent'=>'Faraday v2.9.0'
                 }).
-        to_return(status: 200, body: File.read("spec/fixtures/MrBeast.json"), headers: {})
+        to_return(status: 200, body: File.read("spec/fixtures/MrBeastYoutube.json"), headers: {})
 
         channelId = 'UCX6OQ3DkcsbYNE6H8uQQuVA' # Mr Beast
         query = ''
