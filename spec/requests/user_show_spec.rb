691e0c1a76306888fc1f585e9e889f91db32b8a7
Quin Nordmark <quinnordmark@gmail.com>
2024-03-22 13:37:32 -0600

    User model

---
+++ spec/requests/user_show_spec.rb
@@ -0,0 +1,11 @@
+require "rails_helper"
+
+RSpec.describe "Api::V1::Users", type: :request do
+    before(:each) do
+        @user = User.create(name: "ur brand new stepdad!"
+                            email: "quinnordmark@gmail.com")
+
+        @MrBeast = Creator.create(  name: "MrBeast",
+                                    youtube_handle: "UCX6OQ3DkcsbYNE6H8uQQuVA")
+    end
+end
