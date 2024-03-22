e56ea03389adce7555b20ff9064ddc5b7692c425
Quin Nordmark <quinnordmark@gmail.com>
2024-03-22 13:51:14 -0600

    User Controller, Facade, and Serializer done as far as I can without follows.

---
+++ app/facades/user_facade.rb
@@ -0,0 +1,15 @@
+class UserFacade
+
+    def self.show(user)
+        follows = []
+        user.follows.each do |follow|
+            follows.append({name: follow.name})
+        end
+
+        {
+            'name': user.name
+            'follows': follows
+        }
+    end
+
+end
