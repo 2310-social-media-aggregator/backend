be5414ee7191f1211dbf5dee8b0c68b9ba3bd728
Isaac Mitchell <tmitchellisaac@gmail.com>
2024-03-26 11:35:59 -0600

    Add two follows seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,9 +7,17 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
-@zfg = Creator.create(  name: "ZFG",
+@zfg = Creator.create!(  name: "ZFG",
                         twitch_handle: "8683614",
                         youtube_handle: "UC1qsXgdSxJnQG1wy2Gnvuqw")
 
-user_1 = User.create!( name: "Thomas the Tank Engine",
+@aztecross = Creator.create!(name: "Aztecross",
+                            twitch_handle: "50881182",
+                            youtube_handle: "UClbllR4TxlhYJyrpu1sA4A")
+
+
+@user_1 = User.create!( name: "Thomas the Tank Engine",
                         email: "thomas@gmail.com")
+
+follows_1 = Follow.create!(user_id: @user_1.id, creator_id: @zfg.id)
+follows_2 = Follow.create!(user_id: @user_1.id, creator_id: @aztecross.id)
