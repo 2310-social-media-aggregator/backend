2325226cb85b9aae55523d867a0f0900e84c35d5
Isaac Mitchell <tmitchellisaac@gmail.com>
2024-03-29 15:22:15 -0600

    Add cache method

--- app/facades/youtube_facade.rb
+++ app/facades/youtube_facade.rb
@@ -1,10 +1,9 @@
 class YoutubeFacade
 
-    def self.get_channel(handle, query)
+    def self.get_channel(handle)
         query = '' # delete this line if we are going to add search functionality
-
         youtube_service = YoutubeService.new
-        youtube_json = youtube_service.get_channel(handle, query)
+        youtube_json = youtube_service.get_channel(handle)
         youtube = []
         youtube_json[:items].each do |video|
             youtube.append(YoutubeVideoPoro.new(video))
@@ -12,4 +11,17 @@ class YoutubeFacade
 
         {'videos': youtube}
     end
+
+    def self.cache_get_channel(handle)
+     
+      cache_key = "youtube_#{handle}"
+      Rails.cache.fetch(cache_key, expires_in: 1.hours) do
+
+        get_channel(handle)
+      end
+    
+    end
+
 end
+
+
