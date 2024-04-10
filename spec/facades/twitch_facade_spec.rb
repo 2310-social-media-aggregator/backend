ca5385dbce95d408b7530c7eb416211b5194211e
Isaac Mitchell <tmitchellisaac@gmail.com>
2024-04-10 11:40:00 -0600

    just a test, don't merge

--- spec/facades/twitch_facade_spec.rb
+++ spec/facades/twitch_facade_spec.rb
@@ -25,4 +25,29 @@ RSpec.describe 'TwitchFacade' do
         expect(facade[:videos][0].image).to eq("https://vod-secure.twitch.tv/_404/404_processing_%{width}x%{height}.png")
         expect(facade[:videos][0].id).to eq("2100415767")
     end
+
+    it 'get_channel' do
+      stub_request(:get, "https://api.twitch.tv/helix/videos?first=5&user_id=8683614").
+       with(
+         headers: {
+         'Accept'=>'*/*',
+         'Accept-Encoding'=>'gzip;q=1.0,deflate;q=0.6,identity;q=0.3',
+         'Authorization'=>"Bearer #{Rails.application.credentials.twitch[:bearer_token]}",
+         'Client-Id'=>Rails.application.credentials.twitch[:client_id],
+         'User-Agent'=>'Faraday v2.9.0'
+         }).
+      to_return(status: 200, body: File.read("spec/fixtures/ZFGTwitch.json"), headers: {})
+
+      channelId = '8683614' # ZFG
+      query = ''
+
+      facade = TwitchFacade.get_channel(channelId)
+
+      expect(facade[:videos].count).to be > 1
+      expect(facade[:videos][0]).to be_a(TwitchVideoPoro)
+      expect(facade[:videos][0].published_at).to eq("2024-03-24T19:54:27Z")
+      expect(facade[:videos][0].title).to eq("Majora's Mask Randomizer - No Logic and no starting items")
+      expect(facade[:videos][0].image).to eq("https://vod-secure.twitch.tv/_404/404_processing_%{width}x%{height}.png")
+      expect(facade[:videos][0].id).to eq("2100415767")
+  end
 end
